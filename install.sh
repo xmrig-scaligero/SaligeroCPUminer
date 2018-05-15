dedb669e1c8093a638bd509eaadf10c84d9492f0
Chico Alff <33207386+chicoalff@users.noreply.github.com>
2018-05-15 08:50:45 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get -y update
+sudo apt-get -y install git build-essential cmake libuv1-dev libmicrohttpd-dev
+sudo git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+sudo mkdir build
+cd build
+sudo cmake ..
+sudo make
+cd ..
+sudo cp -R build/ /xmrig-scaligero
+cd /xmrig-scaligero
+sudo wget https://raw.githubusercontent.com/chicoalff/SaligeroCPUminer/master/xmr.json
+
+sudo ./xmrig --config=xmr.json
