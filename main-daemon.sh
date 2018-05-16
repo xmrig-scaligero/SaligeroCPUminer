a74c1b4ab7c2710f56d08ace48428fdfbc4cfea0
Chico Alff <33207386+chicoalff@users.noreply.github.com>
2018-05-16 17:00:41 -0300

    Create main-daemon.sh
    Main daemon miner script

---
+++ main-daemon.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# ScaligeroScript
+# Autor: Chico Alff
+# Version: 0-0-1a
+# Script daemon: main-daemon.sh
+
+
+# START MAIN DAEMON PROCEDURE
+# CHECK IF XMRIG IS ALREADY INSTALLED
+
+cd /
+if [ -d /xmrig-scaligero/pools ]; 
+	then
+    	echo "XNRIG-SCALIGERO ALREADY EXIST"
+    	cd /xmrig-scaligero
+    	sudo rm -R pools
+    	cd ..
+    	sudo mkdir pools
+		cd pools
+		sudo wget https://raw.githubusercontent.com/chicoalff/SaligeroCPUminer/master/xmr.json
+    else 
+    	echo "XNRIG-SCALIGERO DIRECTORY NOT FOUND"
+    	cd /xmrig-scaligero
+    	sudo mkdir pools
+		cd pools
+		sudo wget https://raw.githubusercontent.com/chicoalff/SaligeroCPUminer/master/xmr.json
+		cd ..
+fi
+
+cd /xmrig-scaligero
+sudo ./xmrig --config=pools/xmr.json
+
+# START MAIN MINER SCRIPT
+
+#sudo wget https://raw.githubusercontent.com/chicoalff/SaligeroCPUminer/master/run-xmr.sh
+#sudo sh run-xmr.sh
+
+
+
